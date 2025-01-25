95bd49877623271b5fc91e68eacd672aac326f1c
Omar Ghazi <omghazi@e1r6p8.1337.ma>
2025-01-25 10:30:19 +0100

    post wa9ila

--- Response/Post.cpp
+++ Response/Post.cpp
@@ -1,78 +1,92 @@
 #include "Post.hpp"
 
-Post::Post(RessourceHandler &explorer, Request& request) : explorer(explorer), request(request)
+Post::Post(RessourceHandler &explorer, Request &request) : explorer(explorer), request(request)
 {
-        getCurr_file();
-        writeFile();
+  getCurr_file();
+  writeFile();
 }
 
-Post::Post( const Post &copy ) : explorer(copy.explorer), request(copy.request)
+Post::Post(const Post &copy) : explorer(copy.explorer), request(copy.request)
 {
-        *this = copy;
+  *this = copy;
 }
 
-Post&    Post::operator=(const Post &assign)
+Post &Post::operator=(const Post &assign)
 {
-        if (this != &assign)
-        {
-                this->explorer = assign.explorer;
-                this->request = assign.request;
-        }
-	return *this;
+  if (this != &assign)
+  {
+    this->explorer = assign.explorer;
+    this->request = assign.request;
+  }
+  return *this;
 }
 
 Post::~Post()
 {
   for (size_t i = 0; i < request.__body.size(); i++)
   {
-    close(request.__body[i]._fileName);
-    unlink(request.__body[i]._fileName);
+    unlink(request.__body[i]._fileName.c_str());
   }
 }
 
 void Post::getCurr_file(void)
 {
-    for (size_t i = 0; i < request.__body.size(); i++) {
-        for (size_t j = 0; j < request.__body[i]._headers.size(); j++) {
-            size_t Dispo_pos = request.__body[i]._headers[j].find("Content-Disposition");
-            if (Dispo_pos != std::string::npos)
-            {
-                size_t filenamePos = request.__body[i]._headers[j].find("filename=\"", Dispo_pos);
-                if (filenamePos != std::string::npos)
-                {
-                    size_t startPos = filenamePos + 10;
-                    size_t endPos = request.__body[i]._headers[j].find("\"", startPos);
-                    if (endPos != std::string::npos)
-                    {
-                        std::string fileName = request.__body[i]._headers[j].substr(startPos, endPos - startPos);
-                        curr_file.push_back(fileName);
-                        std::cout << "Extracted file: " << fileName << std::endl;
-                    }
-                }
-            }
+  for (size_t i = 0; i < request.__body.size(); i++)
+  {
+    for (size_t j = 0; j < request.__body[i]._headers.size(); j++)
+    {
+      size_t Dispo_pos = request.__body[i]._headers[j].find("Content-Disposition");
+      if (Dispo_pos != std::string::npos)
+      {
+        size_t filenamePos = request.__body[i]._headers[j].find("filename=\"", Dispo_pos);
+        if (filenamePos != std::string::npos)
+        {
+          size_t startPos = filenamePos + 10;
+          size_t endPos = request.__body[i]._headers[j].find("\"", startPos);
+          if (endPos != std::string::npos)
+          {
+            std::string fileName = request.__body[i]._headers[j].substr(startPos, endPos - startPos);
+            curr_file.push_back(fileName);
+            std::cout << "Extracted file: " << fileName << std::endl;
+          }
         }
+      }
     }
+  }
 }
 
-void    Post::writeFile( void )
+void Post::writeFile(void)
 {
-  t_svec::iterator it1= request._body.begin();
-  t_svec::iterator it2= curr_file.begin();
-  while (it1 != request._body.end() && it2 != curr_file.end())
+  std::vector<s_body>::iterator it1 = request.__body.begin();
+  t_svec::iterator it2 = curr_file.begin();
+  while (it1 != request.__body.end() && it2 != curr_file.end())
   {
     try
     {
-      std::ifstream readFrom(it1->_fileName);
-      std::ostream writeTo(it2);
+      char buff[READ_SIZE];
+      String fullPath = wsu::joinPaths("./uploads", *it2);
+      std::cout << fullPath << std::endl;
+      std::ifstream readFrom(it1->_fileName.c_str(), std::ios_base::binary);
+      std::ofstream writeTo(fullPath.c_str());
       if (!readFrom.is_open())
-        throw std::runtime_error("Can´t open file" + readFrom);
-      if (!writeTo())
-        throw std::runtime_error("Can´t open file" + writeTo);
-      String line;
-      while (std::getline(readFrom, line))
-        writeTo << line + '\n';
+        throw std::runtime_error("Can´t open file");
+      if (!writeTo.is_open())
+      {
+        readFrom.close();
+        throw std::runtime_error("Can´t open file");
       }
-    catch(const std::exception& e)
+      while (readFrom.read(buff, READ_SIZE) || readFrom.gcount() > 0)
+      {
+        std::size_t bytesRead = readFrom.gcount();
+        for (std::size_t i = 0; i < bytesRead; ++i)
+        {
+          writeTo << buff[i];
+        }
+      }
+      readFrom.close();
+      writeTo.close();
+    }
+    catch (const std::exception &e)
     {
       std::cerr << e.what() << '\n';
     }
--- Response/Response.cpp
+++ Response/Response.cpp
@@ -1,25 +1,25 @@
 #include "Response.hpp"
 
 Response::Response(Request &request,
-				   Server &server,
-				   Location &location) : explorer(RessourceHandler(location, request.__URI)),
-										 __server(server),
-										 __request(request),
-										 __location(location)
+                   Server &server,
+                   Location &location) : explorer(RessourceHandler(location, request.__URI)),
+                                         __server(server),
+                                         __request(request),
+                                         __location(location)
 
 {
-	if (wsu::__criticalOverLoad == true)
-		throw ErrorResponse(503, "critical server overload");
-	__check_methods();
-	buildResponse();
+  if (wsu::__criticalOverLoad == true)
+    throw ErrorResponse(503, "critical server overload");
+  __check_methods();
+  buildResponse();
 }
 
 Response::Response(const Response &copy) : explorer(RessourceHandler(copy.explorer)),
-										   __server(copy.__server),
-										   __request(copy.__request),
-										   __location(copy.__location)
+                                           __server(copy.__server),
+                                           __request(copy.__request),
+                                           __location(copy.__location)
 {
-	*this = copy;
+  *this = copy;
 }
 
 Response::~Response()
@@ -28,18 +28,18 @@ Response::~Response()
 
 Response &Response::operator=(const Response &assign)
 {
-	if (this != &assign)
-	{
-		this->__server = assign.__server;
-		this->__request = assign.__request;
-		this->__location = assign.__location;
-		this->fullResponse = assign.fullResponse;
-		this->reasonPhrase = assign.reasonPhrase;
-		this->explorer = assign.explorer;
-		this->headers = assign.headers;
-		this->body = assign.body;
-	}
-	return *this;
+  if (this != &assign)
+  {
+    this->__server = assign.__server;
+    this->__request = assign.__request;
+    this->__location = assign.__location;
+    this->fullResponse = assign.fullResponse;
+    this->reasonPhrase = assign.reasonPhrase;
+    this->explorer = assign.explorer;
+    this->headers = assign.headers;
+    this->body = assign.body;
+  }
+  return *this;
 }
 
 /***********************************************************************
@@ -48,95 +48,95 @@ Response &Response::operator=(const Response &assign)
 
 void Response::__check_methods()
 {
-	if (!__location.__cgiPass.empty())
-		executeCgi();
-	else
-	{
-		switch (__request.__method)
-		{
-		case GET:
-			executeGet();
-			break;
-		case POST:
-			throw ErrorResponse(500, "still working on it");
-			executePost();
-			break;
-		case DELETE:
-			throw ErrorResponse(500, "still working on it");
-			executeDelete();
-			break;
-		default:
-			throw ErrorResponse(405, __location, "Server does not implement this method");
-			break;
-		}
-	}
+  if (!__location.__cgiPass.empty())
+    executeCgi();
+  else
+  {
+    switch (__request.__method)
+    {
+    case GET:
+      executeGet();
+      break;
+    case POST:
+      // throw ErrorResponse(500, "still working on it");
+      executePost();
+      break;
+    case DELETE:
+      throw ErrorResponse(500, "still working on it");
+      executeDelete();
+      break;
+    default:
+      throw ErrorResponse(405, __location, "Server does not implement this method");
+      break;
+    }
+  }
 }
 
 void Response::executeCgi()
 {
-	Cgi cgi(explorer, __request, __location);
-	body = cgi.getBody();
+  Cgi cgi(explorer, __request, __location);
+  body = cgi.getBody();
 }
 
 void Response::executeGet()
 {
-	explorer.loadPathExploring();
-	Get get(__location.__autoindex, explorer);
-	body = get.getBody();
-	code = 200;
-	reasonPhrase = "Ok";
+  explorer.loadPathExploring();
+  Get get(__location.__autoindex, explorer);
+  body = get.getBody();
+  code = 200;
+  reasonPhrase = "Ok";
 }
 
 void Response::executePost()
 {
-	Post post(explorer, __request.__requestbody);
-	body.clear();
-	code = 200;
-	reasonPhrase = "Ok";
+  Post post(explorer, __request);
+  body.clear();
+  code = 200;
+  reasonPhrase = "Ok";
 }
 
 void Response::executeDelete()
 {
-	Delete _delete(explorer);
-	code = 204;
-	reasonPhrase = "No Content";
+  Delete _delete(explorer);
+  code = 204;
+  reasonPhrase = "No Content";
 }
 
 void Response::buildResponse()
 {
-	setHeader();
-	fullResponse = PROTOCOLE_V " " + wsu::intToString(code) + " " + reasonPhrase + "\r\n";
-	for (std::map<String, String>::iterator it = headers.begin(); it != headers.end(); ++it)
-		fullResponse += it->first + ": " + it->second + "\r\n";
-	fullResponse += "\r\n";
-	fullResponse += body;
+  setHeader();
+  fullResponse = PROTOCOLE_V " " + wsu::intToString(code) + " " + reasonPhrase + "\r\n";
+  for (std::map<String, String>::iterator it = headers.begin(); it != headers.end(); ++it)
+    fullResponse += it->first + ": " + it->second + "\r\n";
+  fullResponse += "\r\n";
+  fullResponse += body;
 }
 
 void Response::setHeader()
 {
-	headers["Accept-Ranges"] = "none";
-	headers["Connection"] = "keep-alive";
-	headers["content-length"] = wsu::intToString(body.length());
-	headers["Content-Type"] = wsu::getContentType(explorer.getPath()) + "; charset=UTF-8";
-	headers["server"] = "webserv/1.0";
-	headers["date"] = wsu::buildIMFDate();
+  headers["Accept-Ranges"] = "none";
+  headers["Connection"] = "keep-alive";
+  headers["content-length"] = wsu::intToString(body.length());
+  headers["Content-Type"] = wsu::getContentType(explorer.getPath()) + "; charset=UTF-8";
+  headers["server"] = "webserv/1.0";
+  headers["date"] = wsu::buildIMFDate();
 }
 
 String Response::getResponse() const
 {
-	return fullResponse;
+  return fullResponse;
 }
 
 void Response::print() const
 {
-	std::cout << GREEN << "*******************************************************************\n";
-	std::cout << explorer;
-	std::cout << GREEN << fullResponse << "\n";
-	std::cout << "*****************************Response******************************\n"
-			  << RESET;
+  std::cout << GREEN << "*******************************************************************\n";
+  std::cout << explorer;
+  std::cout << GREEN << fullResponse << "\n";
+  std::cout << "*****************************Response******************************\n"
+            << RESET;
 }
 std::ostream &operator<<(std::ostream &o, const Response &r)
 {
-	r.print();
-	return o;
+  r.print();
+  return o;
 }
