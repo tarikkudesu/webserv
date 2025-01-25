d2d2ad23ecd78395dfeb3c650ce2a167a16d3cfd
om7gh <omghazi@e3r1p14.1337.ma>
2025-01-25 17:02:59 +0100

    test GET

--- srcs/Response/Get.cpp
+++ srcs/Response/Get.cpp
@@ -2,28 +2,28 @@
 
 Get::Get(bool isIndexing, RessourceHandler &explorer) : isIndexing(isIndexing), explorer(explorer)
 {
-        if (explorer.getType() == FILE_)
-                readFile();
-        else if (isIndexing)
-                autoIndexing();
-        else
-                throw ErrorResponse(403, "Forbidden");
+  if (explorer.getType() == FILE_)
+    readFile();
+  else if (isIndexing)
+    autoIndexing();
+  else
+    throw ErrorResponse(403, "Forbidden");
 }
 
 Get::Get(const Get &copy) : explorer(copy.explorer)
 {
-        *this = copy;
+  *this = copy;
 }
 
 Get &Get::operator=(const Get &assign)
 {
-        if (this != &assign)
-        {
-                this->body = assign.body;
-                this->isIndexing = assign.isIndexing;
-                this->explorer = assign.explorer;
-        }
-        return *this;
+  if (this != &assign)
+  {
+    this->body = assign.body;
+    this->isIndexing = assign.isIndexing;
+    this->explorer = assign.explorer;
+  }
+  return *this;
 }
 
 Get::~Get()
@@ -32,36 +32,39 @@ Get::~Get()
 
 String Get::getBody(void)
 {
-        return body;
+  return body;
 }
 
 void Get::readFile(void)
 {
-        std::ifstream file(explorer.getPath().c_str(), std::ios::binary);
-        if (!file.is_open())
-                throw ErrorResponse(500, explorer.__location, "Internal Server Error");
-        std::ostringstream buff;
-        char buffer[4096];
-        while (file.read(buffer, sizeof(buffer)))
-                buff.write(buffer, file.gcount());
-        buff.write(buffer, file.gcount());
-        body = buff.str();
-        buff.clear();
-        file.close();
+  std::ifstream file(explorer.getPath().c_str(), std::ios::binary);
+  if (!file.is_open())
+    throw ErrorResponse(500, explorer.__location, "Internal Server Error");
+  std::ostringstream buff;
+  char buffer[READ_SIZE];
+  while (file.read(buffer, READ_SIZE) || file.gcount() > 0)
+  {
+    std::size_t bytesRead = file.gcount();
+    for (std::size_t i = 0; i < bytesRead; ++i)
+      buff << buffer[i];
+  }
+  body = buff.str();
+  buff.clear();
+  file.close();
 }
 
 void Get::autoIndexing(void)
 {
-        t_svec directories;
-        DIR *dir = opendir(explorer.getPath().c_str());
-        if (!dir)
-                throw ErrorResponse(500, explorer.__location, "could not open directory");
-        struct dirent *entry;
-        while ((entry = readdir(dir)))
-        {
-                directories.push_back(entry->d_name);
-                directories.push_back(" ");
-        }
-        closedir(dir);
-        body = wsu::buildListingBody(explorer.getPath(), directories);
+  t_svec directories;
+  DIR *dir = opendir(explorer.getPath().c_str());
+  if (!dir)
+    throw ErrorResponse(500, explorer.__location, "could not open directory");
+  struct dirent *entry;
+  while ((entry = readdir(dir)))
+  {
+    directories.push_back(entry->d_name);
+    directories.push_back(" ");
+  }
+  closedir(dir);
+  body = wsu::buildListingBody(explorer.getPath(), directories);
 }
